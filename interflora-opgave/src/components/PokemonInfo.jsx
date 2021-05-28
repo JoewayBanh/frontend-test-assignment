05f28240030c34a475c1000cb06351b830326bed
Joeway Banh <joewaybanh@hotmail.com>
2021-05-28 18:49:33 +0200

    added api calls to get a pokemon and added the randomizer btn

---
+++ interflora-opgave/src/components/PokemonInfo.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./PokemonInfo.css";
+const PokemonInfo = () => {
+  const [pokemon, setPokemon] = useState({
+    sprites: {},
+  });
+
+  let randomNum = Math.floor(Math.random() * 152);
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+      .then((res) => setPokemon(res.data));
+  }, []);
+
+  const asd = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+      .then((res) => setPokemon(res.data));
+    console.log("asd");
+  };
+
+  return (
+    <>
+      <div className="pokemon-card">
+        <h1 className="pokemon-name">{pokemon.name}</h1>
+        <div className="pokemon-info">
+          <h4>Pokedex number: {pokemon.id}</h4>
+          <p>Base experience: {pokemon.base_experience}</p>
+          <p>Height: {pokemon.height}0 cm</p>
+          <p>Weight: {pokemon.weight} kg(?)</p>
+        </div>
+        <div>
+          <img src={pokemon.sprites.back_default} alt="front" />
+          <img src={pokemon.sprites.front_default} alt="back" />
+        </div>
+        <button onClick={asd}>Click for random pokemon!</button>
+      </div>
+    </>
+  );
+};
+export default PokemonInfo;
