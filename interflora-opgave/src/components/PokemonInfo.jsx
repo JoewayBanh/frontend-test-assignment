1cc99da32271866995b5075555b6a180f89de7f7
Joeway Banh <joewaybanh@hotmail.com>
2021-10-23 17:50:40 +0200

    reversed back to where it worked again before trying to tweak it

--- interflora-opgave/src/components/PokemonInfo.jsx
+++ interflora-opgave/src/components/PokemonInfo.jsx
@@ -1,84 +1,54 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./PokemonInfo.css";
-
 const PokemonInfo = ({ pokemon, setPokemon }) => {
   const randomNum = Math.floor(Math.random() * 152);
-  const [loading, setLoading] = useState(true);
-
-  const getPokemonList = async () => {
-    let pokemonArr = [];
-    for (let i = 1; i <= 151; i++) {
-      pokemonArr.push(await getPokemonData(i));
-    }
-    console.log(pokemonArr);
-    setPokemon(pokemonArr);
-    setLoading(false);
-    // console.log(pokemon[randomNum].data.sprites);
-  };
-  const getPokemonData = async (randomNum) => {
-    const res = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${randomNum}`
-    );
-    return res;
-  };
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    getPokemonList();
-  }, []);
+    async function fetchData() {
+      await axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+        .then((res) => setPokemon(res.data))
+        .then(setLoaded(true));
+    }
+
+    fetchData();
+  }, [setPokemon]);
 
   const randomizer = async () => {
     await axios
       .get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-      .then((res) => setPokemon(res.data))
-      .then(setLoading(true));
+      .then((res) => setPokemon(res.data));
   };
-  // const toCheck = () => {
-  //   loading ? console.log("it's here") : console.log("not yet");
-  // };
-
-  // toCheck();
+  const toCheck = () =>
+    loaded ? console.log("it's here") : console.log("not yet");
+  toCheck();
   return (
     <>
-      <div className="pokemon-card">
-        {loading ? (
-          <h1>Loading data...</h1>
-        ) : (
+      {loaded && (
+        <div className="pokemon-card">
           <h1 className="pokemon-name">
-            {pokemon[randomNum].data.name}
-            {/* {pokemon[randomNum].data.name + pokemon.name.slice(1)} */}
-            {/* {pokemon.name.charAt(0).toUpperCase() + pokemon.name.substring(1)} */}
+            {/* {!loaded
+              ? "sss"
+              : pokemon.name.charAt(0).toUpperCase() +
+                pokemon.name.substring(1)} */}
+            {pokemon.name}
           </h1>
-        )}
-        {/* <h1 className="pokemon-name">
-          {loading
-            ? "loading data"
-            : pokemon.name.charAt(0).toUpperCase() + pokemon.name.substring(1)}
-          {pokemon.name}
-        </h1> */}
-        <div className="pokemon-info">
-          <h4>Pokedex number: {pokemon.id}</h4>
-          <p>Base experience: {pokemon.base_experience}</p>
-          <p>Height: {(pokemon.height * 0.1).toFixed(1)} m</p>
-          <p>Weight: {(pokemon.weight * 0.1).toFixed(1)} kg</p>
+          <div className="pokemon-info">
+            <h4>Pokedex number: {pokemon.id}</h4>
+            <p>Base experience: {pokemon.base_experience}</p>
+            <p>Height: {(pokemon.height * 0.1).toFixed(1)} m</p>
+            <p>Weight: {(pokemon.weight * 0.1).toFixed(1)} kg</p>
+          </div>
+          <div>
+            <img src={pokemon.sprites.back_default} alt="back" />
+            <img src={pokemon.sprites.front_default} alt="front" />
+          </div>
+          <button onClick={randomizer}>Click for random pokemon!</button>
+          <button onClick={toCheck}>Click to check!</button>
         </div>
-        <div>
-          {/* {loading ? (
-            <p>Loading images...</p>
-          ) : (
-            <img
-              src={pokemon[randomNum].data.sprites.back_default}
-              alt="back"
-            />
-          )} */}
-          {/* <img  src={pokemon[randomNum].data.sprites.back_default} alt="back" /> */}
-          {/* <img src={pokemon.sprites.front_default} alt="front" /> */}
-          {/* <img src={pokemon.sprites.back_default} alt="back" />
-          <img src={pokemon.sprites.front_default} alt="front" /> */}
-        </div>
-        <button onClick={randomizer}>Click for random pokemon!</button>
-        {/* <button onClick={toCheck}>Click to check if API has loaded!</button> */}
-      </div>
+      )}
     </>
   );
 };
